22c5c6b5b0eb021da05b3a7a4fbc6eb93a1d0910
Lovisa Carling <lovisa.carling@gmail.com>
2021-12-15 01:58:19 +0100

    styled booklist component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,70 @@
 import React, { useState, useEffect } from 'react'
-import { BookCard, BookListWrapper } from './StyledComponents'
+import { generatePath, useNavigate } from 'react-router-dom'
+import { BookCard, BookListWrapper, Button } from './StyledComponents'
 import styled from 'styled-components'
+
+const Background = 'https://i.imgur.com/fUBGg4L.jpg'
 
 const BookSection = styled.section`
   width: 80%;
 `
+const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 250px;
+  align-items: center;
+`
+const BookTitleHeader = styled.div`
+  background-color: #7153ff;
+  width: 100%;
+  height: 80px;
+  border-radius: 15px 15px 0px 0px;
+  text-align: center;
+  h2 {
+    font-size: 18px;
+    padding: 0px 10px 0px 10px;
+    color: #fff;
+  }
+`
+const BookInfo = styled.div`
+  display: flex;
+  width: 90%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 10px;
+
+  span {
+    color: #7153ff;
+    font-size: 24px;
+    font-weight: 800;
+  }
+`
+
+const BackgroundImage1 = styled.img`
+  z-index: -1;
+  position: fixed;
+  opacity: 0.6;
+  object-fit: cover;
+  /* top: 20px; */
+  /* left: 50px; */
+  width: 100%;
+  height: 100%;
+  /* transform: rotate(-15deg); */
+`
+const BackgroundImage2 = styled.img`
+  z-index: -1;
+  position: fixed;
+  opacity: 0.3;
+  top: 20px;
+  right: 50px;
+  width: 40%;
+  transform: rotate(15deg);
+`
 
 export const BookList = () => {
   const [bookList, setBookList] = useState([])
+  const navigate = useNavigate()
 
   useEffect(() => {
     fetchBookList()
@@ -20,19 +77,43 @@ export const BookList = () => {
   }
 
   return (
-    <BookSection>
-      <h1>Book List</h1>
-      <BookListWrapper>
-        {bookList.map((book) => (
-          <BookCard key={book.id}>
-            <h2>{book.title}</h2>
-            <h3>{book.authors}</h3>
-            <p>
-              score <span>{book.average_rating}</span>
-            </p>
-          </BookCard>
-        ))}
-      </BookListWrapper>
-    </BookSection>
+    <>
+      <BackgroundImage1 src={Background} alt='girl reading' />
+      <BookSection>
+        <TitleWrapper>
+          <Button
+            onClick={(event) =>
+              navigate(
+                generatePath('/', {
+                  book: event.target.value,
+                })
+              )
+            }
+          >
+            Go back
+          </Button>
+          <h1>Book List</h1>
+        </TitleWrapper>
+
+        <BookListWrapper>
+          {bookList.map((book) => (
+            <BookCard key={book.id}>
+              <BookTitleHeader>
+                <h2>{book.authors}</h2>
+              </BookTitleHeader>
+              <h3>{book.title}</h3>
+              <BookInfo>
+                <p>
+                  pages <span>{book.num_pages}</span>
+                </p>
+                <p>
+                  rating <span>{book.average_rating}</span>
+                </p>
+              </BookInfo>
+            </BookCard>
+          ))}
+        </BookListWrapper>
+      </BookSection>
+    </>
   )
 }
--- src/components/StyledComponents.js
+++ src/components/StyledComponents.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const Button = styled.button`
   padding: 10px;
   width: 110px;
+  height: 40px;
   color: #7153ff;
   margin: 0px 10px 0px 10px;
   border: solid 3px #7153ff;
@@ -15,28 +16,34 @@ export const Button = styled.button`
   }
 `
 export const BookListWrapper = styled.div`
-  /* width: 80%; */
-  /* display: grid;
-  grid-gap: 20px 0px;
-  grid-template-columns: auto; */
   padding: 5px;
-  display: inline-flex;
-  /* flex-direction: row; */
-  flex-wrap: wrap;
-  /* gap: 10px; */
+  grid-gap: 20px;
+  display: grid;
+  grid-template-columns: auto auto;
+
   @media (max-width: 700px) {
-    /* grid-template-columns: 50% 50%;
-    grid-gap: 20px 20px; */
-    flex-direction: column;
+    grid-template-columns: auto;
   }
 `
 
 export const BookCard = styled.div`
-  background-color: #7153ff;
-  /* padding: 40px; */
+  /* padding: 20px 40px 0px 40px; */
+  height: 300px;
+  word-break: break-all;
+  background-color: rgba(255, 255, 255, 0.4);
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   align-items: center;
   flex: 50%;
-  /* margin: 5px; */
+  border-radius: 15px;
+  h3 {
+    font-size: 16px;
+    margin: 0;
+  }
+  span {
+    color: #7153ff;
+    font-size: 24px;
+    font-weight: 800;
+  }
 `
